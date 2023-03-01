66f6065ce6ce5dc6116dc72c4ec17ef37cf32cb7
lucietessy <123073199+lucietessy@users.noreply.github.com>
2023-03-01 23:48:15 +0300

    Add files via upload

---
+++ product_in_functions.c
@@ -0,0 +1,134 @@
+//program to find product of two numbers
+#include<stdio.h>
+/*function declaration */int product();
+int main()
+{
+    /*local variable  definition */
+    int answer;
+    
+   /*calling a function to get product value*/
+    answer = product();
+    printf("the product of the two numbers is:%d\n",answer);
+    return 0;
+}
+/*function returning the product of two numbers*/
+int product()
+{
+    /*local variable definition*/
+    int num1 =2,num2 =3;
+    return num1*num2;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //program to find product of two numbers
+#include<stdio.h>
+/*function declaration */int product();
+int main()
+{
+    /*local variable  definition */
+    int answer;
+    
+   /*calling a function to get product value*/
+    answer = product();
+    printf("the product of the two numbers is:%d\n",answer);
+    return 0;
+}
+/*function returning the product of two numbers*/
+int product()
+{
+    /*local variable definition*/
+    int num1 =2,num2 =3;
+    return num1*num2;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
